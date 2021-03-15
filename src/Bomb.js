4cd8e75149eb19bd8f2cf8eeeeb93c8b343fb993
github username <scott.gloyna01@gmail.com>
2021-03-15 14:24:38 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,33 @@
 // your Bomb code here!
+import React from "react";
+import index from './index.js'
+
+
+
+
+export default class Bomb extends React.Component {
+    constructor(props){
+        super()
+        this.state = {
+          secondsLeft: props.initialCount
+        }
+      }
+      countdown = () => {
+        // console.log(`before setState: ${this.state.secondsLeft}`)
+    
+        this.setState({
+            secondsLeft: this.state.secondsLeft - 1
+        })
+    
+        // console.log(`after setState: ${this.state.count}`)
+        setTimeout(this.countdown,1000)
+      }
+
+    render(){
+        return (
+            <div onClick = {this.countdown}>
+            {this.state.secondsLeft > 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : "Boom"}
+            </div>
+        )
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,18 @@
 // your ImageSlider code here!
+import React from "react";
+
+
+
+export default class ImageSlide extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+          currentSliderIndex: 0
+        }
+      }
+    render(){
+        return (
+            <h1>I am on slide {this.state.currentSliderIndex}</h1>
+        )
+    }
+}
